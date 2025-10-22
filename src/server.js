f7189cca353578fbb8b83cf3f55d80e047c82df2
miggui rivera <migguirivera@gmail.com>
2025-10-22 17:47:00 +0800

    first commit

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "../dist")));
 
 // ✅ SPA fallback (non-API routes)
 app.get(/^\/(?!api|ws).*/, (req, res) => {
-	res.sendFile(path.join(__dirname, "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
 // Start server
